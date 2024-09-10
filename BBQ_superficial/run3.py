fd8603c54b1c9617501aa3477749f262f7661aea
Guangliang Liu <87598718+guangliang-nlp@users.noreply.github.com>
2024-09-10 10:37:26 -0400

    Update run3.py

--- BBQ_superficial/run3.py
+++ BBQ_superficial/run3.py
@@ -1,6 +1,6 @@
 import os
 
-os.environ['CUDA_VISIBLE_DEVICES'] = '2'
+#os.environ['CUDA_VISIBLE_DEVICES'] = '2'
 # os.system("python run_faithful.py --external --wo_unbiased_instruc --max_new_tokens 3 > logs/faithful_bbq_external_wo_unbiased_instruc.log 2>&1")
-os.system("python run_inference.py --cot --external --wo_unbiased_instruc --max_new_tokens 300 > logs/bbq_external_cot_wo_unbiased_instruc.log 2>&1")
-os.system("python run_faithful.py --cot --external --wo_unbiased_instruc --max_new_tokens 300 > logs/faithful_bbq_external_cot_wo_unbiased_instruc.log 2>&1")
+os.system("python run_inference.py --cot --external --wo_unbiased_instruc --max_new_tokens 300 ")#> logs/bbq_external_cot_wo_unbiased_instruc.log 2>&1
+os.system("python run_faithful.py --cot --external --wo_unbiased_instruc --max_new_tokens 300 ")#> logs/faithful_bbq_external_cot_wo_unbiased_instruc.log 2>&1
