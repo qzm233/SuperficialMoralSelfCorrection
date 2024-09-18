2a7f33daf91af6a1ae2e36863a912c2a828fb23e
小芒果 <xiaomangguo@xiaomangguodeMacBook-Air.local>
2024-09-18 12:06:45 +0800

    run2

--- BBQ_superficial/run2.py
+++ BBQ_superficial/run2.py
@@ -7,7 +7,7 @@ import os
 # os.system("python run_faithful.py --wo_unbiased_instruc --max_new_tokens 3")
 # os.system("python run_faithful.py --cot --wo_unbiased_instruc --max_new_tokens 512")
 
-os.system("python run_inference.py --external --max_new_tokens 3")
-os.system("python run_faithful.py --external --max_new_tokens 3")
-os.system("python run_inference.py --cot --wo_unbiased_instruc --max_new_tokens 512")
+# os.system("python run_inference.py --external --max_new_tokens 3")
+# os.system("python run_faithful.py --external --max_new_tokens 3")
+# os.system("python run_inference.py --cot --wo_unbiased_instruc --max_new_tokens 512")
 os.system("python run_faithful.py --cot --wo_unbiased_instruc --max_new_tokens 512")
